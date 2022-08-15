6925c4c72108966e01ea26e2fd1720f5e03d52e0
Abhishek <abhishekagr00008@gmail.com>
2022-08-15 23:03:23 +0530

    added chatbot

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./MainPage.css";
 import "./animate.css";
-
+import { useEffect } from "react";
 import tajmahal from "./images/tajmahal.jpg";
 import charminar from "./images/charminar.jpg";
 import hawamahal from "./images/hawamahal.jpg";
@@ -26,6 +26,23 @@ import ab from "./images2/Agrasen-Ki-Baoli.jpg";
 import at from "./images2/Swaminarayan.jpg";
 
 export default function MainPage() {
+  useEffect(() => {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "174c37c8a10db6579c04456dd95f01450",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  });
   return (
     <>
       <div className="main">
--- src/component/Visit.js
+++ src/component/Visit.js
@@ -6,6 +6,23 @@ import { fetchPlaceList } from "../store/API";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  useEffect(() => {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "174c37c8a10db6579c04456dd95f01450",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  });
   const { placeList } = useSelector((state) => state.place);
   const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(true);
