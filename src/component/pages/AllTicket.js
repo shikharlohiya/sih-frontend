2ddb21db7f60e91b679eb929c2c06b3f486999cf
Abhishek <abhishekagr00008@gmail.com>
2022-08-15 20:49:49 +0530

    payment

--- src/component/pages/AllTicket.js
+++ src/component/pages/AllTicket.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTicket } from "../../store/API";
-
+import "./Alltickket.css";
 function AllTicket() {
   const { data } = useSelector((state) => state.ticket);
   const dispatch = useDispatch();
@@ -10,11 +10,37 @@ function AllTicket() {
   }, [dispatch]);
   console.log(data);
   return (
-    <div>
-      {data?.map((item) => {
-        return <div>{item.price}</div>;
-      })}
-    </div>
+    <>
+      <div className="header-img">
+        <div className="img-pay">
+          <img src="./imagess/Group.png"></img>
+        </div>
+        <p className="thnak">Thank You!</p>
+        <p className="enjoy"> We hope you enjoy your journey.</p>
+      </div>
+      <div className="div-ticket">
+        {data?.map((item) => {
+          return (
+            <div className="cart123 row">
+              <div className="cart-div123 col-3">
+                <img
+                  src={`${process.env.REACT_APP_BACKEND}/${item.monumentId.img}`}
+                  className="cart-img123"
+                ></img>
+              </div>
+              <div className="details-centre col-6">
+                <p className="suc-det">
+                  {item.monumentId.name},{item.monumentId.stateUT}
+                </p>
+              </div>
+              <div className="col-3">
+                <button className="suc-but">View Ticket</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
